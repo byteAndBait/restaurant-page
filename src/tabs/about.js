b6eeb30dbb8f81625710b4f56497fb8da0ab2649
byteAndBait <abdulrahman.elkassas@protonmail.com>
2026-07-14 13:35:47 +0300

    enhance founder page a lil

--- src/tabs/about.js
+++ src/tabs/about.js
@@ -15,17 +15,31 @@ export default function about() {
 
     const founderName = document.createElement("h1")
     founderName.classList.add("founderName")
-    founderName.textContent = "Founder: Gustavo Fring"
+    founderName.textContent = "Founder:Gustavo Fring"
 
     const founderDescription = document.createElement("p")
     founderDescription.classList.add("founderDecription")
     founderDescription.textContent = "Gustavo Fring established Los Pollos Hermanos as a premier fast-food chain specializing in acclaimed Acapulco-style chicken, building the brand from a single location into a beloved regional powerhouse. While he is celebrated as a pillar of the community and a meticulous business leader, his expansive logistical network serves as a highly efficient **conduit** for interests far beyond the culinary world."
+    function makeLink(brandIconName,href)
+    {
+        const linkElement = document.createElement("a")
+        const iconElement = document.createElement("i")
+        iconElement.className = `fa-brands fa-${brandIconName}`;
+        linkElement.href = href
+        linkElement.append(iconElement);
+        return linkElement
+    }
+    const socialLinks = document.createElement("div");
+    socialLinks.classList.add("socialLinks");
+    const facebookLink = makeLink("facebook","https://www.facebook.com/OfficialGiancarloEsposito/")
+    const instagramLink = makeLink("instagram","https://www.instagram.com/thegiancarloesposito/")
+    const twitterLink = makeLink("x-twitter","https://x.com/quiethandfilms")
+    socialLinks.append(facebookLink,instagramLink,twitterLink)
 
-    founderText.appendChild(founderName)
-    founderText.appendChild(founderDescription)
+    founderText.append(founderName,founderDescription,socialLinks)
 
-    container.appendChild(founderImage)
-    container.appendChild(founderText)
+    container.append(founderImage,founderText)
 
     contentElement.appendChild(container)
 }
+
